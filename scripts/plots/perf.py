2665d98e0c1af4b813e1c67d864497876d85d9c0
Jiali Xu <garyxu1001@gmail.com>
2025-04-23 18:50:05 +0200

    feat: add plot script

---
+++ scripts/plots/perf.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+dnn_res = 'backups/Wed_Apr_23_09:42:50_2025_DNN/eval.mat'
+rnn_res = 'backups/Wed_Apr_23_09:40:10_2025_RNN/eval.mat'
+cnn_res = 'backups/Wed_Apr_23_09:37:55_2025_CNN/eval.mat'
+
+# baselines
+tanogan_res = 'backups/tanogan/eval.mat'
+
+dnn_res = loadmat(dnn_res, squeeze_me=True)
+rnn_res = loadmat(rnn_res, squeeze_me=True)
+cnn_res = loadmat(cnn_res, squeeze_me=True)
+
+tanogan_res = loadmat(tanogan_res, squeeze_me=True)
+
+x_axis = [.1, .2, .3, .5, .7, 1., 1.5, 2., 3., 5.]
+baseline = dnn_res['pre-aug-B'][0]
+ec_gan_dnn_acc = [dnn_res[f'ec-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+co_gan_dnn_acc = [dnn_res[f'co-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+
+ec_gan_rnn_acc = [rnn_res[f'ec-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+co_gan_rnn_acc = [rnn_res[f'co-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+
+ec_gan_cnn_acc = [cnn_res[f'ec-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+co_gan_cnn_acc = [cnn_res[f'co-gan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+
+tanogan_acc = [tanogan_res[f'tanogan_B_{int(ratio*100)}'][0] for ratio in x_axis]
+
+x_label = [f'+{int(ratio*100)}%' for ratio in x_axis]
+
+## EC-GAN
+plt.figure(figsize=(10, 6))  # Adjust figure size
+
+plt.plot(x_label, ec_gan_dnn_acc, marker='o', label='EC-GAN-DNN')
+plt.plot(x_label, ec_gan_rnn_acc, marker='x', label='EC-GAN-RNN')
+plt.plot(x_label, ec_gan_cnn_acc, marker='*', label='EC-GAN-CNN')
+
+plt.plot(x_label, tanogan_acc, marker='x', linestyle='--', label='TAnoGAN')
+
+plt.grid(linestyle='--', linewidth=0.5, zorder=0)
+plt.axhline(y=baseline, color='red', linestyle='--', linewidth=2, label='Pre-augmentation')
+plt.ylim(0.7, 1.0)
+# plt.xlim(-.1, 5.1)
+# plt.xticks(x_axis, x_label)
+plt.xlabel("Augmented Data Volume Ratio")  # x-axis label
+plt.ylabel("Accuracy on Dataset B")    # y-axis label
+plt.title("Benchmark Comparison of Different Methods")  # title
+plt.legend()  # show legend
+
+plt.savefig("benchmark_results.png", dpi=800, bbox_inches='tight')
+# plt.savefig("benchmark_results.pdf", dpi=800, bbox_inches='tight')
+
+## CO-GAN
+plt.figure(figsize=(10, 6))  # Adjust figure size
+
+plt.plot(x_label, co_gan_dnn_acc, marker='o', label='CO-GAN-DNN')
+plt.plot(x_label, co_gan_rnn_acc, marker='x', label='CO-GAN-RNN')
+plt.plot(x_label, co_gan_cnn_acc, marker='*', label='CO-GAN-CNN')
+
+plt.plot(x_label, tanogan_acc, marker='x', linestyle='--', label='TAnoGAN')
+
+plt.grid(linestyle='--', linewidth=0.5, zorder=0)
+plt.axhline(y=baseline, color='red', linestyle='--', linewidth=2, label='Pre-augmentation')
+plt.ylim(0.7, 1.0)
+# plt.xlim(-.1, 5.1)
+# plt.xticks(x_axis, x_label)
+plt.xlabel("Augmented Data Volume Ratio")  # x-axis label
+plt.ylabel("Accuracy on Dataset B")    # y-axis label
+plt.title("Benchmark Comparison of Different Methods")  # title
+plt.legend()  # show legend
+
+plt.savefig("benchmark_results_co.png", dpi=800, bbox_inches='tight')
+# plt.savefig("benchmark_results.pdf", dpi=800, bbox_inches='tight')
